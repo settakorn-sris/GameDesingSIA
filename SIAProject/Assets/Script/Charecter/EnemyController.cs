385c51ea9ee8ac37ca260762fcf14be13163bdca
KasiditR <77877339+KasiditR@users.noreply.github.com>
2021-09-11 15:30:33 +0700

    DistanceEnemyPlayer

--- SIAProject/Assets/Script/Charecter/EnemyController.cs
+++ SIAProject/Assets/Script/Charecter/EnemyController.cs
@@ -6,6 +6,7 @@ public class EnemyController : MonoBehaviour
     [SerializeField] public NavMeshAgent agent;
     [SerializeField] private Transform player;
     [SerializeField] private EnemyCharecter enemy;
+    [SerializeField] private float distanceStopEnemy;
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -13,6 +14,23 @@ public class EnemyController : MonoBehaviour
     }
     void Update()
     {
+        float dis = Vector3.Distance(transform.position, player.position);
+        if (dis <= distanceStopEnemy)
+        {
+            StopGoToPlayer();
+        }
+        else
+        {
+            GoToPlayer();
+        }
+    }
+    void GoToPlayer()
+    {
+        agent.isStopped = false;
         agent.SetDestination(player.position);
+    }
+    void StopGoToPlayer()
+    {
+        agent.isStopped = true;
     }
 }
