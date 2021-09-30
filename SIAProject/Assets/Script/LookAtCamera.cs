541b3f7ea3b9f3ab5d075d370294d272f9d6f217
janjao937 <61728559+janjao937@users.noreply.github.com>
2021-09-30 21:01:47 +0700

    Delete Comment

--- SIAProject/Assets/Script/LookAtCamera.cs
+++ SIAProject/Assets/Script/LookAtCamera.cs
@@ -11,11 +11,6 @@ public class LookAtCamera : MonoBehaviour
     }
     void Update()
     {
-        //Vector3 v = cameraToLookAt.transform.position - transform.position;
-        //v.x = v.z = 0.0f;
-        //transform.LookAt(cameraToLookAt.transform.position - v);
-        //transform.Rotate(0, 180, 0);
-
         transform.LookAt(transform.position + cameraToLookAt.transform.rotation * new Vector3(0, 0, -1), cameraToLookAt.transform.rotation * new Vector3(0, 1, 0));
     }
 }
